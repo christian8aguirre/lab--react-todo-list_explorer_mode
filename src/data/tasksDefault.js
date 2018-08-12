748ac55fd18bb2a7c6f6f4ec33383c8c2a27cf3f
christian8aguirre <christian8.aguirre@icloud.com>
2018-08-12 15:58:28 -0500

    Error with my 'Remove Item'

--- src/data/tasksDefault.js
+++ src/data/tasksDefault.js
@@ -1,29 +1,36 @@
 export const tasksDefault = [{
   key: 1,
   text:'wash cat',
-  date : '2001-01-01',
-  critical: true,
+  date : '2016-08-11',
+  critical: false,
   days: 0,
   isRemoved: false
 },{
   key: 2,
   text:'read book',
-  date : '2010-01-02',
-  critical: false,
+  date : '2016-08-03',
+  critical: true,
   days: 0,
   isRemoved: false
 },{
   key: 3,
   text:'rethink my life',
-  date : '2011-01-03',
+  date : '2016-08-05',
   critical: true,
   days: 0,
   isRemoved: false
 },{
   key: 4,
   text:'go for a swim',
-  date : '2019-01-04',
+  date : '2016-08-12',
   critical: false,
+  days: 0,
+  isRemoved: false
+},{
+  key: 5,
+  text:'buy food for goldfish',
+  date : '2016-08-27',
+  critical: true,
   days: 0,
   isRemoved: false
 }]
